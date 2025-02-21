2ecd548d011c090b83d3f7957dd36a7726784c39
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-21 22:23:59 +0530

    Both element return index 2

--- lowlevel/numpy_array/array_slicing/2d_array_slice.py
+++ lowlevel/numpy_array/array_slicing/2d_array_slice.py
@@ -3,3 +3,4 @@ import numpy as np
 arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
 print("From the second element, slice elements from index 1 to index 4 :",arr[1,1:4])
+print("From both element return index 2 :" ,arr[0:2 ,2])
