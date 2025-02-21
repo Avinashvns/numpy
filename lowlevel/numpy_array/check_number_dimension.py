2ee6624dfbf523f90dcba2cca9bfb16acbf05b2d
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-21 21:48:35 +0530

    Check Number of Dimension ( ndim )

---
+++ lowlevel/numpy_array/check_number_dimension.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a=np.array(21)
+b=np.array([1,2,3,4])
+c=np.array([[1,2,3],[4,5,6]])
+d=np.array([[[1,2,3],[4,5,6]],[[9,8,7],[6,5,4]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
